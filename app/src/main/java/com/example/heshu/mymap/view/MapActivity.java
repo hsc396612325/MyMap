5805f38f1695f4bb6c2f2224469d419427c720cc
hsc396612325 <heshucheng@xiyou3g.com>
2018-02-10 21:25:06 +0800

    添加按钮的UI

--- app/src/main/java/com/example/heshu/mymap/view/MapActivity.java
+++ app/src/main/java/com/example/heshu/mymap/view/MapActivity.java
@@ -5,11 +5,18 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
@@ -26,22 +33,36 @@ import com.example.heshu.mymap.bean.LocationBean;
 import com.example.heshu.mymap.presenter.MapPresenter;
 import com.example.heshu.mymap.util.ImageUtil;
 
-public class MapActivity extends BaseActivity implements IMapView{
+public class MapActivity extends BaseActivity implements IMapView,View.OnClickListener{
 
-    MapPresenter mPresenter;
+    private MapPresenter mPresenter;
     private  MapView mMapView;
     private BaiduMap mBaiduMap;
-    private static final int BAIDU_READ_PHONE_STATE =100;
+    private Button mAddButton;
+    private Button mRecommendButton;
+    private Button mNavigationButton;
 
+    private PopupWindow mPopuWindow1;
+    private View mContentView1;
+    private static final int BAIDU_READ_PHONE_STATE =100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPresenter = new MapPresenter(this);
-
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
+
+        initView();
+    }
+    private void initView(){
         mMapView = (MapView)findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
+        mAddButton =(Button)findViewById(R.id.addButton);
+        mRecommendButton = (Button)findViewById(R.id.recommendButton);
+        mNavigationButton = (Button)findViewById(R.id.navigationButton);
+        mAddButton.setOnClickListener(this);
+        mRecommendButton.setOnClickListener(this);
+        mNavigationButton.setOnClickListener(this);
 
         if(ContextCompat.checkSelfPermission(App.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED){
@@ -51,6 +72,7 @@ public class MapActivity extends BaseActivity implements IMapView{
         }
 
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -67,6 +89,28 @@ public class MapActivity extends BaseActivity implements IMapView{
     protected void onPause() {
         super.onPause();
         mMapView.onPause();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.addButton:
+                initPopuWindow1(view);
+                break;
+            case R.id.recommendButton:
+                break;
+            case R.id.navigationButton:
+                break;
+            case R.id.commentButton:
+                break;
+            case R.id.imageButton:
+                break;
+            case R.id.voiceButton:
+                break;
+            case R.id.videoButton:
+                break;
+            default:break;
+        }
     }
 
     //显示定位
@@ -140,5 +184,32 @@ public class MapActivity extends BaseActivity implements IMapView{
             default:
                 break;
         }
+    }
+
+    private void initPopuWindow1(View parent) {
+        if (mPopuWindow1 == null) {
+            LayoutInflater mLayoutInflater = LayoutInflater.from(this);
+            mContentView1 = mLayoutInflater.inflate(R.layout.popupwindow1, null);
+            mPopuWindow1 = new PopupWindow(mContentView1, ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+
+        Button commentButton = (Button)mContentView1.findViewById(R.id.commentButton);
+        Button imageButton = (Button)mContentView1.findViewById(R.id.imageButton);
+        Button voiceButton = (Button)mContentView1.findViewById(R.id.voiceButton);
+        Button videoButton = (Button)mContentView1.findViewById(R.id.videoButton);
+
+        commentButton.setOnClickListener(this);
+        imageButton.setOnClickListener(this);
+        voiceButton.setOnClickListener(this);
+        videoButton.setOnClickListener(this);
+
+        ColorDrawable cd = new ColorDrawable(0x000000);
+        mPopuWindow1.setBackgroundDrawable(cd);
+        mPopuWindow1.setOutsideTouchable(true);
+        mPopuWindow1.setFocusable(true);
+        mPopuWindow1.showAtLocation((View)parent.getParent(), Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 0);
+
+        mPopuWindow1.update();
     }
 }
